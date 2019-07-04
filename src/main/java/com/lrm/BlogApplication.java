264d8d0b8c237edb58c2146afd5c5581fb66d6ba
caifenghua <caifenghua@cmbi.com.hk>
2019-07-04 19:46:18 +0800

    添加ssl证书

--- src/main/java/com/lrm/BlogApplication.java
+++ src/main/java/com/lrm/BlogApplication.java
@@ -1,15 +1,24 @@
 package com.lrm;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.WebApplicationInitializer;
 
 import javax.swing.*;
 
 @SpringBootApplication
-public class BlogApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+public class BlogApplication extends SpringBootServletInitializer implements WebApplicationInitializer , EmbeddedServletContainerCustomizer {
 
     @Override
     protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
@@ -19,4 +28,42 @@ public class BlogApplication extends SpringBootServletInitializer implements Web
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+
+
+    //拦截所有请求
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    //配置http转https
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(80);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(443);
+        return connector;
+    }
+
+    //这里设置默认端口为443，即https的，如果这里不设置，会https和http争夺80端口
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(443);
+    }
 }
