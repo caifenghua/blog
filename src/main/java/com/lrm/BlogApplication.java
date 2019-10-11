487c33a2c7dc24408cbbce05e2a358dce4c5d1c2
caifenghua <caifenghua@cmbi.com.hk>
2019-10-11 16:26:44 +0800

    更换一下背景图

--- src/main/java/com/lrm/BlogApplication.java
+++ src/main/java/com/lrm/BlogApplication.java
@@ -18,7 +18,7 @@ import org.springframework.web.WebApplicationInitializer;
 import javax.swing.*;
 
 @SpringBootApplication
-public class BlogApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+public class BlogApplication extends SpringBootServletInitializer implements WebApplicationInitializer , EmbeddedServletContainerCustomizer {
 
     @Override
     protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
@@ -30,5 +30,40 @@ public class BlogApplication extends SpringBootServletInitializer implements Web
 	}
 
 
+    //拦截所有请求
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
 
+    //配置http转https
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(80);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(443);
+        return connector;
+    }
+
+    //这里设置默认端口为443，即https的，如果这里不设置，会https和http争夺80端口
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(443);
+    }
 }
